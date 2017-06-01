44cc060a74fea97e85a97aab41e5ef54d2caa950
kasanion <kasanion@PHMADKASANION1>
2017-06-01 10:52:57 +0800

    Add project files.

---
+++ PopQuiz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PopQuiz.MvcApplication" Language="C#" %>
---
+++ PopQuiz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PopQuiz.Startup))]
+namespace PopQuiz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
